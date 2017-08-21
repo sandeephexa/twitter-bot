b3f824f7b2b492508305c8c1a6e8a71bbccfef9f
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-08-21 14:40:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	    consumer_key: 'xdVCqpK8VQFxuGbhbakZ4y1lT ',
+	    consumer_key: 'xdVCqpK8VQFxuGbhbakZ4y1lT',
 	   consumer_secret: '6a0HAY7R4WUI055cBs3Td7kc23hqWZf1Q7O1MsvLzhxxypDDxB',
 	    access_token: '899327533153267712-GuVdC2eQ150MwZHnWKBryIf5AhBoZWz',
 	    access_token_secret: 'C0XNn79y2ZY8iOMbBBSgoDUwasAroSXhB03A4Hnb0G2MM'
