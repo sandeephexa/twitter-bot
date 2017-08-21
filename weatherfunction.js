6eef98a9118b1258f8d7df91f52bf28de754a5aa
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-08-21 14:18:48 +0530

    Update weatherfunction.js

--- weatherfunction.js
+++ weatherfunction.js
@@ -1,14 +1,20 @@
-exports.WelcomeParams = function (sender_id, screen_name) {
+exports.WelcomeParams = function (sender_id, screen_name, media_id) {
     return {
         "event": {
             "type": "message_create",
             "message_create": {
                 "target": {
                     "recipient_id": sender_id
-                }, "message_data": {
-                    "text": "Hi," + screen_name + "\n\nWelcome to our Textile Company!!!\n\nPlease Select the Category",
-                   
-                    
+                },
+                "message_data": {
+                    "text": "Hi," + screen_name + "\n\nWelcome to weather forecast !!!\n\n",
+
+                    "attachment": {
+                        "type": "media",
+                        "media": {
+                            "id": media_id
+                        }
+                    }
                 }
             }
         }
