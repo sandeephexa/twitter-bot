ba62bf9b25e81d85b3fe44edc17c5b06d02d1fff
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-08-21 17:18:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ stream.on('direct_message', function (directMsg) {
                // var image_media = JSON.parse(uploadMedia.TwitterUpload());
                 paramssend = botfunction.WelcomeParams(sender_id, screen_name);
                 Twitter.post("direct_messages/events/new", paramssend, function (err, data, response) {
-                    stream.stop();
-                    stream.start();
+                    //stream.stop();
+                   // stream.start();
                 })
             }
             
